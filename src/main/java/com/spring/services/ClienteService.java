7380a0957cd332607bb055078b13cfa45b56ef3c
Walter Lemus <walter.lemus@hotmail.com>
2020-12-06 15:51:22 -0600

    se agregaron los metodos restantes en el cliente service

--- src/main/java/com/spring/services/ClienteService.java
+++ src/main/java/com/spring/services/ClienteService.java
@@ -7,5 +7,11 @@ import com.spring.entity.Cliente;
 public interface ClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
 
 }
--- src/main/java/com/spring/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/spring/services/impl/ClienteServiceImpl.java
@@ -21,6 +21,25 @@ public class ClienteServiceImpl implements ClienteService{
 	public List<Cliente> findAll() {  
 		return (List<Cliente>) clienteDAO.findAll();
 	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDAO.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDAO.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDAO.deleteById(id);
+		
+	}
 	
 
 }
