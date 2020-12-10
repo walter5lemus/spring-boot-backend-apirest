1be568414577cd329feac84dae441dbb265a986e
Walter Lemus <walter.lemus@hotmail.com>
2020-12-10 00:28:29 -0600

    se completó el crud para clientes

--- src/main/java/com/spring/controller/ClienteController.java
+++ src/main/java/com/spring/controller/ClienteController.java
@@ -1,11 +1,19 @@
 package com.spring.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.entity.Cliente;
@@ -23,5 +31,37 @@ public class ClienteController {
 	public List<Cliente> index(){
 		return clienteService.findAll();
 	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setApellidos(cliente.getApellidos());
+		clienteActual.setEmail(cliente.getEmail());
+		clienteActual.setNombres(cliente.getNombres());
+		
+		return clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
+	
+	
+	
 
 }
