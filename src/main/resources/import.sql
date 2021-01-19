7b5467e91c060adf3b946d3e8802696b7a1485f2
Walter Lemus <walter.lemus@hotmail.com>
2021-01-18 22:48:08 -0600

    nuevos cambios para deploy

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,9 +18,9 @@ INSERT INTO clientes (nombres, apellidos, email, create_at, region_id) VALUES('T
 INSERT INTO clientes (nombres, apellidos, email, create_at, region_id) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', TO_DATE('2018-03-06','yyyy-MM-dd'),1);
 
 /* Creamos algunos usuarios con sus roles */
-INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('andres','$2a$10$PzcLk21IT2OKWpII3iXNXOIRwaIISECpe/sKvdRE/OwK1w43o3Jom',1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com' );
-INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('walter','$2a$10$/BXNcYLb8O9MmTo88rxqoeS0D6x1.bOZD6JJ9dkbnEwMhzkDQU7E6',1,'Walter','Lemus','walterlemus@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('admin','$2a$10$gLDoCGnSr8HT4dsmBgNzaeyRkK58fP2VKGWgQSaA/vKMhO/m.2v2S',1,'ADMIN','ADMIN','ADMIN@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('andres','$2a$10$PzcLk21IT2OKWpII3iXNXOIRwaIISECpe/sKvdRE/OwK1w43o3Jom',true, 'Andres', 'Guzman', 'profesor@bolsadeideas.com' );
+INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('walter','$2a$10$/BXNcYLb8O9MmTo88rxqoeS0D6x1.bOZD6JJ9dkbnEwMhzkDQU7E6',true,'Walter','Lemus','walterlemus@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombres, apellidos, email) VALUES('admin','$2a$10$gLDoCGnSr8HT4dsmBgNzaeyRkK58fP2VKGWgQSaA/vKMhO/m.2v2S',true,'ADMIN','ADMIN','ADMIN@gmail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
@@ -30,20 +30,20 @@ INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (3,2);
 
 /* tabla producto*/
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD 43 inc', 399, sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara digital DSC-W320B', 300, sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple Ipod Shuffle', 150, sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook z110', 650, sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('HP Multifuncional F2280', 199.99, sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi bicicleta Aro 26', 125,sysdate);
-INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica comoda 5 cajones', 89.99, sysdate);
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD 43 inc', 399, now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara digital DSC-W320B', 300, now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple Ipod Shuffle', 150, now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook z110', 650, now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('HP Multifuncional F2280', 199.99, now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Bianchi bicicleta Aro 26', 125,now());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Mica comoda 5 cajones', 89.99, now());
 
 /* creamos algunas facturas*/
-INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos de oficina', null, 1, sysdate);
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura equipos de oficina', null, 1, now());
 INSERT INTO factura_items ( cantidad, factura_id, producto_id ) VALUES (1,1,1);
 INSERT INTO factura_items ( cantidad, factura_id, producto_id ) VALUES (2,1,4);
 INSERT INTO factura_items ( cantidad, factura_id, producto_id ) VALUES (1,1,5);
 INSERT INTO factura_items ( cantidad, factura_id, producto_id ) VALUES (1,1,7);
 
-INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura bicicleta', 'Alguna nota importante', 1, sysdate);
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Factura bicicleta', 'Alguna nota importante', 1, now());
 INSERT INTO factura_items ( cantidad, factura_id, producto_id ) VALUES (3,2,6);
