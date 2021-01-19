6c491d9cc607de3a0f297f944910c43028c94d36
Walter Lemus <walter.lemus@hotmail.com>
2021-01-18 22:13:53 -0600

    solucion de error del cors y prepara ambiente para heroku

--- src/main/java/com/spring/auth/ResourceServerConfig.java
+++ src/main/java/com/spring/auth/ResourceServerConfig.java
@@ -34,7 +34,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Content-type","Authorization"));
